8afe603721d0ed11fe770d061c6a37ccd52671e2
Diptanil Sen <150945082+Diptanil-Sen@users.noreply.github.com>
2026-07-13 18:39:08 +0530

    Update sample_data.dart

--- lib/data/sample_data.dart
+++ lib/data/sample_data.dart
@@ -3,7 +3,6 @@ import 'package:jugaad_fix/models/jugaad_model.dart';
 class JugaadCategories {
   static const allKey = 'all';
 
-  
   static const categories = <Map<String, String>>[
     {'key': 'power', 'label': 'Power Cut', 'emoji': '🔌'},
     {'key': 'internet', 'label': 'Internet', 'emoji': '📶'},
